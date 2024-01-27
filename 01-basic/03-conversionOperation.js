dc86c3066698d3215b828fa34dbbf485516499c0
syed habeeb ali <114045436+Mr-habe3b@users.noreply.github.com>
2024-01-27 10:55:59 +0000

    "conversion cleared"

--- 01-basic/03-conversionOperation.js
+++ 01-basic/03-conversionOperation.js
@@ -1,9 +1,8 @@
-
-```number , string conversion```
+//"number , string conversion"
 
 let Mark = 60;
 // value of Mark is number 
-let MarkLevel = "exellents"
+let MarkLevel = "exellents";
 
 //console.log(typeof Mark);// ONE way to check the value type "Mark"
 //console.log(typeof(Mark));// SECOND way to check the value type "Mark"
@@ -22,13 +21,48 @@ let ValueInNumber = Number (MarkLevel);
 //"exellent" => NaN
 // "true" => 1 : "false" => 0
 
-``` boolean conversion ```
+//" boolean conversion "
 
 let IsLoggedIn = "mrhabe3b";
 let booleanIsLoggedIn = Boolean(IsLoggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 => true : 0 => false
 // "" => false
 // "mrhabe3b" => true
 
+// `````````````````````operators````````````````````````````
+
+let value = 2;
+let minusValue = -value;
+//console.log(minusValue);
+
+// console.log(2+2); //"+" to add
+// console.log(2-2);  // "-" to subract 
+// console.log(2*2); // "*" to multiply
+// console.log(2/2); // "/" to divide
+// console.log(2%2); // "%" to  get reminder
+
+
+//using operator in string 
+let str1 = "welcome";
+let str2 = " mrhabe3b";
+
+let str3 = str1 + str2;
+
+console.log(str3);
+
+// console.log("1" + 2); //recall the documentation of best possible answer
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 +"2");
+
+// console.log(3+4 * 5 % 3);// it is bad practice in production work
+
+// console.log(+true);// it is aslo bad practice 
+// console.log(+"");// it is aslo bad practice 
+
+let gameController = 404;
+//++gameController;//prefix operator
+gameController++;// postfix operator
+console.log(gameController);
