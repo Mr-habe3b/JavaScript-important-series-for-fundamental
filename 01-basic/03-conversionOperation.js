4e54cb825c8fda2b462061a8b9322d79cbf75392
syed habeeb ali <114045436+Mr-habe3b@users.noreply.github.com>
2024-01-27 10:13:57 +0000

    'conversion cleared'

---
+++ 01-basic/03-conversionOperation.js
@@ -0,0 +1,34 @@
+
+```number , string conversion```
+
+let Mark = 60;
+// value of Mark is number 
+let MarkLevel = "exellents"
+
+//console.log(typeof Mark);// ONE way to check the value type "Mark"
+//console.log(typeof(Mark));// SECOND way to check the value type "Mark"
+
+let ValueInString = String (Mark);
+//Here we convert number to string
+//console.log(typeof ValueInString);
+//console.log(ValueInString);
+
+let ValueInNumber = Number (MarkLevel);
+//here we convert string to number 
+//console.log(typeof ValueInNumber);
+//console.log(ValueInNumber);
+
+// 60 = "66" 
+//"exellent" => NaN
+// "true" => 1 : "false" => 0
+
+``` bolean conversion ```
+
+let IsLoggedIn = "mrhabe3b";
+let booleanIsLoggedIn = Boolean(IsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true : 0 => false
+// "" => false
+// "mrhabe3b" => true
+
