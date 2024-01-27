11f35d46499275a4d4f5464e870a6f1549e38f3d
syed habeeb ali <114045436+Mr-habe3b@users.noreply.github.com>
2024-01-27 09:02:41 +0000

    'variable cleared'

---
+++ 01-basic/variable.js
@@ -0,0 +1,21 @@
+const variableId= 1;
+let variableEmail = "Mrhabe3b@google.com";
+var variblePassword = "123456789";
+let variableState = "Telangana";
+
+
+/* - is the bad practice  */
+variableCity ="hyderabad";
+
+//
+//variableId = 2;
+/*
+prefer not to use "var"
+because of issue in block scope and function scope
+
+*/
+console.log(variableId);
+
+console.log(variableCity);
+
+console.table([variableId,variableEmail,variblePassword,variableState,variableCity]);
