bf2c3845ee6f92b5009abfad252aa95e354b3217
syed habeeb ali <114045436+Mr-habe3b@users.noreply.github.com>
2024-01-30 19:53:34 +0000

    "date"

---
+++ 01-basic/08-date.js
@@ -0,0 +1,9 @@
+//dates
+
+let myDate = new Date();
+console.log(myDate.toString());//output "Tue Jan 30 2024 19:38:32 GMT+0000 (Coordinated Universal Time)"
+console.log(myDate.toDateString());//output "Tue Jan 30 2024"
+console.log(myDate.toLocaleDateString());//output "1/30/2024"
+console.log(typeof myDate);//type of mydate object
+
+
