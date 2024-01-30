326b45da613f148c4f20cee632485e890fb1d129
syed habeeb ali <114045436+Mr-habe3b@users.noreply.github.com>
2024-01-30 19:31:13 +0000

    "number-maths cleared"

--- 01-basic/07-number-maths.js
+++ 01-basic/07-number-maths.js
@@ -19,9 +19,19 @@ const hundered = 1000000;
 
 //``````````````````` Maths ```````````````````````
 
-console.log(Math);
+// console.log(Math);
 //only negative value change to position value but positive will never to negative
-console.log(Math.abs(-4));
-console.log(Math.round(4,6));
-console.log(Math.ceil(4,2));
-console.log(Math.floor(4,9));
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,3,6,8));
+// console.log(Math.max(4,3,6,8));
+
+// console.log(Math.random()); // maths.random value will between 0 to 1
+// console.log(Math.floor(Math.random()*10) + 1); 
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1))+ min);
